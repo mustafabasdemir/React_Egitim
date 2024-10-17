060845ed43bc9dc911000215de6c547e81795b74
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2024-10-18 00:37:53 +0300

    components eklendi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//components icinde tanımlı ogreyı ımport ettik
+
+import Baslik from "./components/baslik";   
+ 
+function App() {
+  return (
+    <div className="App">
+      <Baslik/>   
+      {/* componnets icindeki basligi cagirdik */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/baslik.jsx
@@ -0,0 +1,15 @@
+
+
+const Baslik = () => {
+    return(
+        <div>baslik</div>
+    )
+}
+
+export default Baslik
+
+
+/*
+components içinde bir çok sayfada kullanilacak yapıları yapıp dısarıya export ediyoruz 
+her bir components icin farkli farkli yapı olsuturmak mantıklı 
+*/
